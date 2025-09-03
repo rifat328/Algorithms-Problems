8c37afb615a9610aef7c4cee9c63fb3bcb052ac1
Rifat Hossain <rifathossain328663@gmail.com>
2025-09-03 19:19:27 +0600

    Implemented Queue using two stacks with push, pop, peek, and empty methods old syntex

---
+++ DSA Implementation/queueUsing2Stack.js
@@ -0,0 +1,56 @@
+var MyQueue = function () {
+  this.stack1 = [];
+  this.stack2 = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.stack2.length === 0) {
+    // push everything from stack 1 to stack 2 this way we can remove the first element because it gets reversed
+    // it will give us an average time complexity since we dont always pull data from stack 1 , if we did that that would we o(n)
+    while (this.stack1.length > 0) {
+      this.stack2.push(this.stack1.pop());
+    }
+  }
+  return this.stack2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.stack2.length === 0) {
+    while (this.stack1.length > 0) {
+      this.stack2.push(this.stack1.pop());
+    }
+  }
+  return this.stack2[this.stack2.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  if (this.stack1.length === 0 && this.stack2.length === 0) {
+    return true;
+  } else return false;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
