627f622112e26b5e4536abac8c64539324ac8ec7
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-09-07 20:06:10 +0600

    Time: 0 ms (100.00%) | Memory: 57.6 MB (80.49%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,25 +4,40 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let n= nums.length
-    let halfIndex=Math.floor(n/2)
-    let mid= nums[halfIndex]
-    if(nums.length === 1){
-        return nums[0] === target? 0: -1
-    }else if(target < mid ){
-        for(let i=0; i< halfIndex; i++){
-            if(nums[i] === target){
-                return i
-            }
-        }
-        return -1
-    }else{
-        for(let i=halfIndex; i< n; i++){
-            if(nums[i] === target){
-                return i
-            } 
-        }
-        return -1
-
+    let left =0,right= nums.length -1 // points to 2 ends of the array
+    while(left <= right){
+        let mid= Math.floor((left+right) /2) //mid calculation on each iteration
+        if (nums[mid] === target) { return mid}
+        else if(nums[mid] < target) {left= mid +1}
+        else right = mid-1
     }
+    return -1
 };
+
+
+// var search = function(nums, target) {
+//     let n= nums.length
+//     let halfIndex=Math.floor(n/2)
+//     let mid= nums[halfIndex]
+//     if(nums.length === 1){
+//         return nums[0] === target? 0: -1
+//     }else if(target < mid ){
+//         for(let i=0; i< halfIndex; i++){
+//             if(nums[i] === target){
+//                 return i
+//             }
+//         }
+//         return -1
+//     }else{
+//         for(let i=halfIndex; i< n; i++){
+//             if(nums[i] === target){
+//                 return i
+//             } 
+//         }
+//         return -1
+
+//     }
+// }
+
+// Time: O(log n) because the search space is halved each step.
+// Space: O(1) if iterative, O(log n) if recursive.
