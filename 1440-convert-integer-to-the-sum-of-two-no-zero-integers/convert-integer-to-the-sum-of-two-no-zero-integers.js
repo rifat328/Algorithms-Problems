cf51f81ef2df4ddb0fdef5195bdf33a09b47b64a
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-09-08 21:34:07 +0600

    Time: 0 ms (100.00%) | Memory: 54.5 MB (23.19%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -2,25 +2,36 @@
  * @param {number} n
  * @return {number[]}
  */
- let hasZeroDigit= function(n){
-    while(n>0){
-        if(n%10 === 0) return true;
-        n= Math.floor(n/10)
-    }
-    return false;
- }
-var getNoZeroIntegers = function(n) {
-    let stack=[]
-    let count=0
-    for (let i = 1; i < n; i++) {
-        let a = i, b = n - i;
-        if (!hasZeroDigit(a) && !hasZeroDigit(b)) {
-      return [a, b];  // return the pair immediately
-        }
-    }
-        
-    }
+ 
 
+var getNoZeroIntegers = function(n) {
+    let hasZero = (num)=> !String(num).includes(0)
+    for (let a = 1; a < n; a++) {
+         b = n - a;
+        if(hasZero(a) && hasZero(b)){
+            return [a, b];
+        }
+ 
+    }
+}
+
+// 2nd solution beats 100% 0ms runtime , memory 43.48%
+// let hasZeroDigit= function(n){
+//     while(n>0){
+//         if(n%10 === 0) return true;
+//         n= Math.floor(n/10)
+//     }
+//     return false;
+//  }
+// var getNoZeroIntegers = function(n) {
+//     for (let i = 1; i < n; i++) {
+//         let a = i, b = n - i;
+//         if (!hasZeroDigit(a) && !hasZeroDigit(b)) {
+//       return [a, b];  
+//         }
+//     }
+        
+//     }
 // my initial solution 4ms beats 14.49%
 // let hasZeroDigit= function(n){
 //     while(n>0){
