7c63a2749c41c32e24e29c5f4ace5b4cf1aad01d
Rifat Hossain <rifathossain328663@gmail.com>
2025-09-13 11:17:35 +0600

    Implement Binary Search Tree with insert and lookup methods

---
+++ DSA Implementation/binarySearchTree.js
@@ -0,0 +1,81 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value == current.value) {
+          return undefined; // to avoid duplicate values
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else if (value == current.value) {
+        return current;
+      }
+    }
+    return false;
+  }
+  //remove(value) {}
+}
+const tree = new BinarySearchTree();
+
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+// console.log(JSON.stringify(traverse(tree.root)));
+console.log(JSON.stringify(traverse(tree.root), null, 4));
+
+//console.log(traverse(tree.root));
+//      9
+//   4     20
+// 1  6  15 170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
