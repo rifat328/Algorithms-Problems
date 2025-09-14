aec8693ede66346f050829888c867ce1d6bd73a9
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-09-14 21:28:28 +0600

    Time: 0 ms (100.00%) | Memory: 63.2 MB (5.23%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    let current= root
+    while(current){
+         if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else if (val == current.val) {
+        return current;
+      }
+    }
+    return null;
+};
+
