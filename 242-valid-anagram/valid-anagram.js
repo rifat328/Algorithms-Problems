cc18d9015042187c3c292918109aa945ebaa159c
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-09-16 22:56:13 +0600

    Time: 5 ms (95.76%) | Memory: 56.9 MB (74.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -15,3 +15,21 @@ return count.every(x => x === 0);
 }
 
 
+// solution 1 
+// var isAnagram = function(s, t) {
+   
+//     if (s.length !== t.length) {
+//         return false;
+//     }
+//     let map = new Map();
+//     // Count characters in s
+//     for (let i = 0; i < s.length; i++) {
+//         map.set(s[i], (map.get(s[i]) || 0) + 1)
+//     }
+//     // Subtract characters using t
+//     for (let i = 0; i < t.length; i++) {
+//          if (!map.has(t[i]) || map.get(t[i]) <= 0) return false;
+//         map.set(t[i], map.get(t[i]) - 1);
+//     }
+//     return true;
+// };
