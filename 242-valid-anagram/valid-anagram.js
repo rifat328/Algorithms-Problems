2a81387397f9db71e83726b5b0e56473256bf4e9
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-09-16 21:52:48 +0600

    Time: 14 ms (59.59%) | Memory: 57.6 MB (59.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -13,16 +13,13 @@ var isAnagram = function(s, t) {
 
     // Count characters in s
     for (let i = 0; i < s.length; i++) {
-        map.set(s[i], (map.get(s[i]) || 0) + 1);
+        map.set(s[i], (map.get(s[i]) || 0) + 1)
     }
 
     // Subtract characters using t
     for (let i = 0; i < t.length; i++) {
-        if (!map.has(t[i]) || map.get(t[i]) <= 0) {
-            return false;
-        }
+         if (!map.has(t[i]) || map.get(t[i]) <= 0) return false;
         map.set(t[i], map.get(t[i]) - 1);
     }
-
     return true;
 };
