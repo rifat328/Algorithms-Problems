dfef44937c9de3187b0790a85c9d3204d2d87188
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-10-04 21:52:48 +0600

    Time: 3 ms (84.33%) | Memory: 57.8 MB (98.50%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.js
+++ 437-path-sum-iii/path-sum-iii.js
@@ -12,6 +12,32 @@
  * @return {number}
  */
 var pathSum = function(root, targetSum) {
+   let prefixSum = new Map()
+   prefixSum.set(0,1) // base case: empty path sum = 0
+
+   function dfs(node,currentSum){
+    if(!node){return 0};
+    currentSum+= node.val;
+    // Check how many paths ending at this node have sum = targetSum
+    let count = prefixSum.get( currentSum - targetSum) || 0
+    // Add current prefix sum to map
+    prefixSum.set(currentSum, (prefixSum.get(currentSum) || 0 ) +1)
+
+    count+= dfs(node.left, currentSum)
+    count+= dfs(node.right, currentSum)
+    // Backtrack: remove this node’s prefix sum before going back up
+     prefixSum.set(currentSum, prefixSum.get(currentSum) - 1);
+     return count;
+   }
+
+    return dfs(root, 0)
+
+};
+
+
+/*
+
+var pathSum = function(root, targetSum) {
     // n Square solution because its calculating for every possible path
     function countPathFromNode(node, target){
         if(!node){ return 0}
@@ -33,3 +59,5 @@ var pathSum = function(root, targetSum) {
 
 
 };
+
+*/
