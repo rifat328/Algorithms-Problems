46bda49393ddb53cc07f4eca34cff86624a48cb6
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-10-23 09:53:45 +0600

    Time: 64 ms (45.71%) | Memory: 66.3 MB (14.82%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -13,8 +13,8 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     if(!root || root === p || root === q) return root;
-    let left = lowestCommonAncestor(root.left, p, q);
-    let right = lowestCommonAncestor(root.right, p, q);
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
     if(left && right)return root;
     return left ? left : right;
 };
