3676a0e2cf8bfcd315d2ca81b170d0b05f9f87d6
Rifat Hossain <rifathossain328663@gmail.com>
2025-11-09 19:26:40 +0600

    Implement Fibonacci calculation using iterative and recursive methods

---
+++ Algorithms/fibonacci.js
@@ -0,0 +1,43 @@
+// write two function the calculate factorial of a given number ,
+// function 1 should use Iterative Approach  and function 2 should use recursion
+
+let findFibonacciIterative = (number) => {
+  if (number <= 1) return number;
+  // so, when number is 0 or 1 then its number value 0 and 1 . so we can ignore these 2 cheks
+  // so we can start from 2 , to 5 (if input is 5)
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= number; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
+};
+
+let findFibonacciRecursive = (number) => {
+  if (number <= 1) return number;
+  return (
+    findFibonacciRecursive(number - 1) + findFibonacciRecursive(number - 2)
+  );
+  //  fib(5)
+  //  ├── fib(4)
+  //  │    ├── fib(3)
+  //  │    │    ├── fib(2)
+  //  │    │    │    ├── fib(1) → 1
+  //  │    │    │    └── fib(0) → 0
+  //  │    │    └── fib(1) → 1
+  //  │    └── fib(2)
+  //  │         ├── fib(1) → 1
+  //  │         └── fib(0) → 0
+  //  └── fib(3)
+  //       ├── fib(2)
+  //       │    ├── fib(1) → 1
+  //       │    └── fib(0) → 0
+  //       └── fib(1) → 1
+};
+
+console.log(findFibonacciIterative(43)); // ✅ Pass O(N)
+
+console.log(findFibonacciRecursive(43)); // ✅ Pass O(2 ^ N)
