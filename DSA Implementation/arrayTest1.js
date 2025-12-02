cf3a8da56ae60d5d4eb639574fc6e7f231a50899
Rifat Hossain <rifathossain328663@gmail.com>
2025-12-02 11:33:26 +0600

    Add initial implementation of graph data structure with examples

---
+++ DSA Implementation/arrayTest1.js
@@ -0,0 +1,9 @@
+let graph = new Array(3).fill([]);
+let graph2 = Array.from({ length: 3 }, () => []);
+
+graph[0].push(1);
+graph[1].push(2);
+console.log(graph);
+// ---------------------------
+graph2[0].push(1);
+console.log(graph2);
