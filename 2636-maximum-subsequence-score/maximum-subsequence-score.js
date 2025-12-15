1779c4e07847b5d13fdd6a8642dd365b763229eb
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-12-15 17:52:40 +0600

    Time: 461 ms (5.55%) | Memory: 95.8 MB (59.26%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.js
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.js
@@ -6,60 +6,58 @@
  */
 var maxScore = function(nums1, nums2, k) {
     // Step 1: Pair nums2 with nums1 and sort by nums2 descending
-    const pairs= nums2.map((val, index)=> [val, nums1[index]]);
-    pairs.sort((a,b)=>b[0] - a[0]);
+    const pairs = nums1.map((val, index)=>[nums2[index], val])
     // Now pairs = [ [nums2, nums1], ... ] sorted by nums2 high → low
-
+    pairs.sort((a,b)=> b[0] - a[0] );
     // create min-heap for k largest nums1 values
     class MinHeap{
         constructor(){
-            this.heap=[];
+            this.heap = [];
         }
-
         push(x){
             this.heap.push(x);
-            this.bubbleUp(this.heap.length -1);
+            this.bubbleUp(this.heap.length -1)
         }
         pop(){
-            if(this.heap.length === 1 ){return this.heap.pop()}
-            const top = this.heap[0];
+            if(this.heap.length === 1) return this.heap.pop();
+            let top = this.heap[0];
             this.heap[0] = this.heap.pop();
             this.bubbleDown(0);
             return top;
         }
+        bubbleUp(index){
+            
+            while(index > 0 ){
+                let parent = Math.floor((index - 1) / 2);
+                if(this.heap[parent] < this.heap[index]) break;
+                [this.heap[parent],this.heap[index]] = [this.heap[index], this.heap[parent]]
+                index = parent;
+            }
+        }
+        bubbleDown(index){
+            const n = this.heap.length;
+            while(true){
+                let left = index*2 +1;
+                let right= index*2 +2;
+                let smallest = index;
+
+                if(left < n && this.heap[left] < this.heap[smallest]) smallest = left;
+                if(right < n && this.heap[right] < this.heap[smallest]) smallest = right;
+
+                if ( smallest === index ) break;
+                [this.heap[index] , this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+                index= smallest;
+            }
+
+        }
         size(){
             return this.heap.length;
         }
-        bubbleUp(i){
-            while(i>0){
-                let parent= Math.floor((i-1)/2);
-                if(this.heap[parent] <= this.heap[i] ) break;
-                [this.heap[parent], this.heap[i]]= [this.heap[i], this.heap[parent]];
-                i= parent;
-            }
-        }
-        bubbleDown(i){
-            const n = this.heap.length;
-            while(true){
-                let left = i*2 +1;
-                let right= i*2 +2;
-                let smallest = i;
-
-                if ( left < n && this.heap[left] < this.heap[smallest]) smallest = left;
-                if(right< n && this.heap[right] < this.heap[smallest]) smallest = right;
-
-                if ( smallest === i ) break;
-                [this.heap[i] , this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
-                i= smallest;
-            }
-        }
-
-        
     }
 
     const heap = new MinHeap();
         let sum =0;
-        result = 0;
+        let result = 0;
          // iterate over pairs.
         for(const [curMinNums2, curNums1] of pairs){
             // add nums1 to heap
@@ -72,7 +70,7 @@ var maxScore = function(nums1, nums2, k) {
             }
             //if heap exactly k elements , compute score
             if(heap.size() === k ){
-                result = Math.max(result, sum * curMinNums2);
+              result = Math.max(result, sum * curMinNums2);
                 
             }
         }
