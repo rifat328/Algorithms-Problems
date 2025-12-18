053328d4b1cc1c84c6969aee7e9e7e1dedf39021
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-12-18 14:38:15 +0600

    Time: 133 ms (18.07%) | Memory: 87.1 MB (18.07%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
@@ -5,27 +5,26 @@
  * @return {number[]}
  */
 var successfulPairs = function(spells, potions, success) {
-    potions.sort((a,b)=> a -b);
-    const result= [];
-    let m= potions.length;
-    for (let s of spells){
-        let minPotion = Math.ceil(success / s);
-        // Binary search for first potion >= minPotion
-        let left =0;
-        let right = m-1;
-        let index=m;
-        while(left <= right){
-            let mid = Math.floor((left + right) / 2);
-            if(potions[mid] >= minPotion){
-                index = mid;
-                right = mid -1;
+   potions.sort((a,b)=> a - b);
+   let m = potions.length;
+   let result=[];
 
+   for(let s of spells){
+        let minPotion = Math.ceil(success/s);
+        let left = 0;
+        let right= m -1;
+        let index = m
+        while(left <= right){
+            let mid =Math.floor((left + right) / 2) ;
+            if(potions[mid] >= minPotion){
+                index = mid
+                right = mid -1
+                
             }else{
-                left = mid + 1;
+                left = mid +1;
             }
         }
-        result.push(m - index);
-    }
-
-    return result;
+        result.push(m - index)
+   }
+   return result;
 };
