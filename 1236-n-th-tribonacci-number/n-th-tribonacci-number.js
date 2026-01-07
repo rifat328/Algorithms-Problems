dc12b83f528529139a2ad62170ec0c16d48ebc28
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2026-01-07 22:14:00 +0600

    Time: 0 ms (100.00%) | Memory: 55.5 MB (5.38%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
@@ -3,14 +3,27 @@
  * @return {number}
  */
 var tribonacci = function(n) {
+    // space optimized version
     if(n === 0){return 0};
     if (n === 1 || n === 2) return 1;
-    const dp = new Array(n + 1);
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 1;
+    let a=0,b=1,c=1;
     for(let i=3; i<=n; i++){
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        let next= a+b+c;
+        a=b;
+        b=c;
+        c=next;
     }
-    return dp[n];
+    return c;
 };
+
+// normal dp version
+    // if(n === 0){return 0};
+    // if (n === 1 || n === 2) return 1;
+    // const dp = new Array(n + 1);
+    // dp[0] = 0;
+    // dp[1] = 1;
+    // dp[2] = 1;
+    // for(let i=3; i<=n; i++){
+    //     dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    // }
+    // return dp[n];
