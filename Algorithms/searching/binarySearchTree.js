988c77a9be7f5a0b2acb170ea00311d52822ef2c
Rifat Hossain <rifathossain328663@gmail.com>
2026-02-11 08:51:13 +0600

    traversal quiz about BFS and DFS , and binarySearchTree added to Algorithms

---
+++ Algorithms/searching/binarySearchTree.js
@@ -0,0 +1,149 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value == current.value) {
+          return undefined; // to avoid duplicate values
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  lookup(value) {
+    //search
+    if (!this.root) {
+      return false;
+    }
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else if (value == current.value) {
+        return current;
+      }
+    }
+    return false;
+  }
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        } else if (currentNode.right.left === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            currentNode.right.left = currentNode.left;
+            // if parent > current , make rihgt child of the left the parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+
+              //if parent < current, make right child a right child of parent
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.rihgt;
+            }
+          }
+          //  option 3 Right child that has a left child.
+        } else {
+          // find the right child's left most child
+          let leftmost = currentNode.right.left;
+          let leftmostParent = currentNode.right;
+          while (leftmost.left != null) {
+            leftmostParent = leftmost;
+            leftmost = leftmost.left;
+          }
+          // Parents left subtree is now leftmost's right subtree
+          leftmostParent.left = leftmost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+          if (parentNode === null) {
+            this.root = leftmost;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost;
+            }
+          }
+        }
+        return true;
+      }
+    }
+  }
+}
+const tree = new BinarySearchTree();
+
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+// console.log(JSON.stringify(traverse(tree.root)));
+console.log(JSON.stringify(traverse(tree.root), null, 4));
+console.log("value: ", tree.lookup(20));
+//console.log(traverse(tree.root));
+//      9
+//   4     20
+// 1  6  15 170
+tree.remove(170);
+console.log(JSON.stringify(traverse(tree.root), null, 4));
+function traverse(node) {
+  // to print the tree structure
+  // DFS traversal
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
---
+++ Algorithms/searching/traversal quiz.md
@@ -0,0 +1,23 @@
+### If you know a solution is not far from the root of the tree:
+
+    ** Bredth First Search **
+
+### If the tree is very deep and solution are rare
+
+    ** BFS (DFS will take so much time and solution is rare ,because the tree is very deep) **
+
+### If the tree is very wide
+
+    **DFS (BFS might take up to much memory **
+
+### IF solutions are frequent but located deep in the tree
+
+    ** DFS **
+
+### Determining whether a path exist between two nodes
+
+    **DFS**
+
+### Finding the shortest Path
+
+    **BFS**
