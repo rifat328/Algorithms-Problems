aa8ed282942dabd1ffe894009313bea23c141193
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2026-02-16 19:52:08 +0600

    Time: 26 ms (97.24%) | Memory: 73.6 MB (11.57%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -5,28 +5,26 @@
 var threeSum = function(nums) {
     //two pointer approach.
     nums.sort((a,b)=>a-b);
-    const res=[];
-    for(let i=0; i<nums.length; i++){
-        let x=nums[i];
-        if(x > 0) break;
-        if(i>0 && nums[i] === nums[i-1]) continue; 
-        let left= i+1;
-        let right= nums.length-1;
-        while(left < right){
-
-            let value = x + nums[left] + nums[right];
-            if(value == 0){
-                res.push([ x, nums[left], nums[right] ])
-                left++;
-                right--;
-                while(left<right && nums[left] === nums[left-1]){left++}
-            }else if(value < 0){
-                left++;
-            }else{
-                right--;
-            }
+   let res=[]
+   for(let i=0; i< nums.length; i++){
+    let left= i+1;
+    let right=nums.length -1;
+    let x = nums[i] ;
+    if(x> 0) break;
+    if(i>0 && nums[i] == nums[i-1]) continue;
+    while(left < right){
+        let sum = x + nums[left] + nums[right];
+        if(sum == 0){
+           res.push([x,nums[left], nums[right]])
+           left++;
+           right--;
+           while(left<right && nums[left] == nums[left-1]){left++ }
+        }else if(sum < 0){
+            left++;
+        }else{
+            right--;
         }
-        
     }
-    return res;
+   }
+   return res;
 };
