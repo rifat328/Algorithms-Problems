e42a8c3de2cc2a4a7cf7b499b1cde955f62d6a6b
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2026-03-22 15:08:12 +0600

    Time: 8 ms (35.05%) | Memory: 60.1 MB (30.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -5,7 +5,7 @@
 var trap = function(height) {
    //brute force precomputation
    let n= height.length
-   if(n == 0 ){
+   if(n === 0 ){
     return 0;
    }
    let leftMax= new Array(n).fill(0);
