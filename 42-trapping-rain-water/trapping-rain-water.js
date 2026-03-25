87fa83d9f1ed778a717f80da8aa7ef4420981aa8
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2026-03-25 13:13:08 +0600

    Time: 0 ms (100.00%) | Memory: 55.8 MB (54.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -3,30 +3,26 @@
  * @return {number}
  */
 var trap = function(height) {
-   //brute force precomputation
+   //two pointer approach
    let n= height.length
    if(n === 0 ) return 0;
-   let leftMax= new Array(n).fill(0);
-   let rightMax=new  Array(n).fill(0);
-   let max=0;
-   // for leftMax at each position of i
-   for(let i=1; i<n; i++){
-   max= Math.max(max , height[i-1])
-   leftMax[i]=(max)
-   }
-   max = 0;
-   for(let i=n-2; i>0; i--){
-   max= Math.max(max , height[i+1])
-   rightMax[i]=(max);
-   }
-   let total= 0
-   for(let i= 0; i< n; i++){
-    let min = Math.min(leftMax[i], rightMax[i])
-    let water = min - height[i];
-    if(water >0){
-        total+= water
+   let left=0;let right= n-1;
+   let leftMax=height[left]
+   let rightMax=height[right]
+   
+   let output=0;
+    // max wall on that side – height at that position
+   while(left < right){
+    if(leftMax <= rightMax){
+        left++;
+        leftMax = Math.max(leftMax, height[left])
+        output += leftMax - height[left];
+    }else if(leftMax > rightMax){
+        right--;
+        rightMax= Math.max(rightMax, height[right])
+        output += rightMax - height[right]
     }
-    
    }
-   return total;
+   return output;
+   
 };
